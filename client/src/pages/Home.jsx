63bdcbf2434eb32e97c443a2e0e555e84b2eab11
Pratishtha103 <11f23pratishtha@gmail.com>
2025-09-17 13:57:33 +0530

    Initial commit: MERN food delivery app

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import api from '../services/Api';
+import RestaurantCard from '../components/RestaurantCard';
 
 function Home() {
   const [restaurants, setRestaurants] = useState([]);
